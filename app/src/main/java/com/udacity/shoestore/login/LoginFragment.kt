60e06bf92ea9a07eac3aa016acbb49215346d796
Mara Rosa <edimara.rosa@outlook.com>
2022-11-22 23:24:26 -0300

    create navigation file and login screen

---
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -0,0 +1,45 @@
+package com.udacity.shoestore.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.signupButton.setOnClickListener {
+            Toast.makeText(
+                requireContext(),
+                "sign up",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        binding.loginButton.setOnClickListener {
+            Toast.makeText(
+                requireContext(),
+                "log in",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
